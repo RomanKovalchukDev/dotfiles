a46a7eb1a4e1670f871b90dff5eb9dce62437894
Roman Kovalchuk <romankovalchuk@nerdzlab.com>
2026-03-23 21:13:34 +0200

    Move Brewfile to machine-setup/mac/ and remove bin subdirectory
    - Move Brewfile from machine-setup/mac/bin/ to machine-setup/mac/
    - Remove machine-setup/mac/bin/ directory
    - Update install.sh to reference new Brewfile path
    
    Final machine-setup/mac/ structure:
    - bootstrap.sh
    - install.sh
    - Brewfile
    - set-defaults.sh
    - set-hostname.sh

--- machine-setup/mac/install.sh
+++ machine-setup/mac/install.sh
@@ -7,7 +7,7 @@ set -e
 cd "$(dirname $0)"/../..
 
 echo "› brew bundle"
-brew bundle --file=machine-setup/mac/bin/Brewfile
+brew bundle --file=machine-setup/mac/Brewfile
 
 # find the installers and run them iteratively
 find config/mac -name install.sh | while read installer ; do sh -c "${installer}" ; done
