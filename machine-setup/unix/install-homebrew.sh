78b6cd5827eda26159de85d12c6d4a72c6ab61d7
Roman Kovalchuk <romankovalchuk@nerdzlab.com>
2026-03-24 13:35:50 +0200

    Update Homebrew installation to use current method
    - Replace deprecated ruby installer with modern bash installer
    - Use HEAD instead of master branch (master returns 404)
    - Add Homebrew to PATH after installation for current session
    - Support both macOS locations (/opt/homebrew and /usr/local)
    - Support Linux location (/home/linuxbrew)
    
    Fixes 'curl: (56) The requested URL returned error: 404' during
    Homebrew installation.

--- machine-setup/unix/install-homebrew.sh
+++ machine-setup/unix/install-homebrew.sh
@@ -11,13 +11,22 @@ then
   echo "  Homebrew not found."
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
+  # Install Homebrew (works on both macOS and Linux)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Add Homebrew to PATH for this session
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # macOS: Homebrew is typically in /opt/homebrew or /usr/local
+    if test -d "/opt/homebrew/bin"; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif test -d "/usr/local/bin/brew"; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    # Linux: Homebrew is typically in /home/linuxbrew
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   fi
 else
   echo "  Homebrew already installed at $(which brew)"
